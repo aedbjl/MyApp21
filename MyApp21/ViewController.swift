f977d7bfc90cdb355c4dbfd5b47b1d560cd85982
iii-user <iii-user@iii-userde-MacBook-4.local>
2017-07-04 13:54:05 +0800

    j

--- MyApp21/ViewController.swift
+++ MyApp21/ViewController.swift
@@ -7,19 +7,81 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func camera(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            let imgPC = UIImagePickerController()
+            imgPC.sourceType = .camera
+            imgPC.delegate = self
+            
+            show(imgPC, sender: self)
+        }else {
+            print("XX")
+        }
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let img = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = img
+        
+        
+        dismiss(animated :true, completion:{() in
+            print("OK")
+        })
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated :true, completion:{() in
+            print("Cancel")
+        })
+    }
+        
+    @IBAction func test1(_ sender: Any) {
+        
+        let videoURL = URL(string:"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
 
+        let player = AVPlayer(url: videoURL!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        view.layer.addSublayer(playerLayer)
+        player.play()
+        
+    }
+    
+    @IBAction func test2(_ sender: Any) {
+        
+        let videoURL = URL(string:"https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+        
+        let player = AVPlayer(url: videoURL!)
+        let playerVC = AVPlayerViewController()
+        playerVC.player = player
+        
+        self.present(playerVC, animated: true)
+
+        
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
+
 
